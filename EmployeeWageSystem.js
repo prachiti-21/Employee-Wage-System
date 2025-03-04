b29e1b1a630a294bab219ab3d59b81ed82f56142
Prachiti <prachitiagarkar1@gmail.com>
2025-03-04 10:18:51 +0530

    [Prachiti Agarkar] add: calculating employee wage

--- EmployeeWageSystem.js
+++ EmployeeWageSystem.js
@@ -1,8 +1,25 @@
-const IS_PRESENT = 1;
-let empCheck = Math.floor(Math.random() * 2); // Generates 0 or 1
+const PART_TIME = 1;
+const FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
 
-if (empCheck === IS_PRESENT) {
-    console.log("Employee is Present");
-} else {
-    console.log("Employee is Absent");
+let empCheck = Math.floor(Math.random() * 3); // Generates 0, 1, or 2
+let empHours = 0;
+
+switch (empCheck) {
+    case PART_TIME:
+        empHours = PART_TIME_HOURS;
+        console.log("Employee is working Part Time");
+        break;
+    case FULL_TIME:
+        empHours = FULL_TIME_HOURS;
+        console.log("Employee is working Full Time");
+        break;
+    default:
+        empHours = 0;
+        console.log("Employee is Absent");
 }
+
+let empWage = empHours * WAGE_PER_HOUR;
+console.log(`Daily Employee Wage: $${empWage}`);

00a69dc986d9063f4ce7d28f6f71b9012270caf0
Prachiti <prachitiagarkar1@gmail.com>
2025-03-04 10:25:30 +0530

    [Prachiti Agarkar] add: calculate wages for a month

--- EmployeeWageSystem.js
+++ EmployeeWageSystem.js
@@ -3,6 +3,7 @@ const FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const WORKING_DAYS_PER_MONTH = 20;
 
 function getWorkHours(empCheck) {
     switch (empCheck) {
@@ -15,9 +16,15 @@ function getWorkHours(empCheck) {
     }
 }
 
-let empCheck = Math.floor(Math.random() * 3); // Generates 0, 1, or 2
-let empHours = getWorkHours(empCheck);
-let empWage = empHours * WAGE_PER_HOUR;
+let totalEmpHours = 0;
 
-console.log(`Employee worked ${empHours} hours`);
-console.log(`Daily Employee Wage: $${empWage}`);
+for (let day = 1; day <= WORKING_DAYS_PER_MONTH; day++) {
+    let empCheck = Math.floor(Math.random() * 3); // Generates 0, 1, or 2
+    let empHours = getWorkHours(empCheck);
+    totalEmpHours += empHours;
+}
+
+let monthlyWage = totalEmpHours * WAGE_PER_HOUR;
+
+console.log(`Total Hours Worked in a Month: ${totalEmpHours}`);
+console.log(`Monthly Employee Wage: $${monthlyWage}`);

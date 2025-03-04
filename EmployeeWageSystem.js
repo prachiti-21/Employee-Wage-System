b695f93961d3c7ccd838bc88df239c4518506f6f
Prachiti <prachitiagarkar1@gmail.com>
2025-03-04 10:21:58 +0530

    [Prachiti Agarkar] add: add functions

--- EmployeeWageSystem.js
+++ EmployeeWageSystem.js
@@ -4,22 +4,20 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empCheck = Math.floor(Math.random() * 3); // Generates 0, 1, or 2
-let empHours = 0;
-
-switch (empCheck) {
-    case PART_TIME:
-        empHours = PART_TIME_HOURS;
-        console.log("Employee is working Part Time");
-        break;
-    case FULL_TIME:
-        empHours = FULL_TIME_HOURS;
-        console.log("Employee is working Full Time");
-        break;
-    default:
-        empHours = 0;
-        console.log("Employee is Absent");
+function getWorkHours(empCheck) {
+    switch (empCheck) {
+        case PART_TIME:
+            return PART_TIME_HOURS;
+        case FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
 
+let empCheck = Math.floor(Math.random() * 3); // Generates 0, 1, or 2
+let empHours = getWorkHours(empCheck);
 let empWage = empHours * WAGE_PER_HOUR;
+
+console.log(`Employee worked ${empHours} hours`);
 console.log(`Daily Employee Wage: $${empWage}`);
